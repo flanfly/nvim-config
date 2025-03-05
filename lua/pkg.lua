a2e91db298b0b5f5ff18b9561c90ec9bae9880cd
Kai Michaelis <kai.michaelis@immune.gmbh>
2025-03-05 21:00:29 +0800

    nicer lsp hilights

--- lua/pkg.lua
+++ lua/pkg.lua
@@ -280,6 +280,16 @@ return require('lazy').setup({
             crust = "#1b1b1b",
           },
         },
+        custom_highlights = function(colors)
+          local u = require("catppuccin.utils.colors")
+          return {
+            CursorLineNr = { bg = u.blend(colors.overlay0, colors.base, 0.75), style = { "bold" } },
+            CursorLine = { bg = u.blend(colors.overlay0, colors.base, 0.45) },
+            LspReferenceText = { bg = colors.surface2 },
+            LspReferenceWrite = { bg = colors.surface2 },
+            LspReferenceRead = { bg = colors.surface2 },
+          }
+        end,
       })
 
       vim.cmd("colorscheme catppuccin-mocha")
