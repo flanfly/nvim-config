2c66b02efaed82fd5a538760b30d518b46975a0f
Kai Michaelis <kai@binarly.io>
2025-08-22 20:44:14 +0800

    clean up treesitter

--- lua/tree.lua
+++ lua/tree.lua
@@ -1,35 +1,12 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = {
-    "c",
-    "lua",
-    "go",
-    "beancount",
-    "html",
-    "hcl",
-    "json",
-    "svelte",
-    "tsx",
-    "markdown",
-    "markdown_inline",
-    "typescript",
-    "yaml",
-    "proto",
-    "make",
-    "javascript",
-    "python",
-    "gomod",
-    "graphql",
-    "dockerfile",
-    "cpp",
-    "bash"
-  },
+  ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = {},
+  ignore_install = { "help", "ipkg" },
 
   highlight = {
     -- `false` will disable the whole extension
@@ -39,35 +16,21 @@ require('nvim-treesitter.configs').setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = {
-      "c",
-      "lua",
-      "go",
-      "beancount",
-      "html",
-      "json",
-      "tsx",
-      "markdown",
-      "markdown_inline",
-      "typescript",
-      "yaml",
-      "rust",
-      "proto",
-      "make",
-      "javascript",
-      "python",
-      "gomod",
-      "graphql",
-      "dockerfile",
-      "cpp",
-      "bash"
-    },
+    -- disable = {},
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
 
     -- disabled for catppuccino compatibility
     additional_vim_regex_highlighting = false,
   },
   incremental_selection = {
-    enable = false,
+    enable = true,
     keymaps = {
       init_selection = "<leader>si",
       node_incremental = "<leader>sn",
