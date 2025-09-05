84877fee57c565ba241aef78b1dc1a45892aa426
Kai Michaelis <kai@binarly.io>
2025-09-05 18:34:47 +0800

    configure keys

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("config.lazy")
+require("config.keys")
 require("config.options")
---
+++ lua/config/keys.lua
@@ -0,0 +1,6 @@
+-- Disable defaults
+pcall(vim.keymap.del, "n", "gra")
+pcall(vim.keymap.del, "n", "grt")
+pcall(vim.keymap.del, "n", "gri")
+pcall(vim.keymap.del, "n", "grn")
+pcall(vim.keymap.del, "n", "grr")
