e22817ceae369e89107a4760c26047ddd526ac91
Kai Michaelis <kai@binarly.io>
2025-09-22 19:52:25 +0800

    add mini-ai to get af

---
+++ lua/plugins/mini-ai.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-mini/mini.ai',
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      -- Set up custom text objects for Treesitter
+      custom_textobjects = {
+        -- 'f' for function. You can use any key you like.
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+        A = ai.gen_spec.treesitter({ a = "@assignment.outer", i = "@assignment.inner" }),
+      },
+    }
+  end,
+}
